96f5d9355b9213ccf042323bf1d02c13fe4c11dc
Shizobear <K.Tobi@outlook.de>
2018-10-13 17:52:50 +0200

    added more funfair prefabs

---
+++ GameJam/Assets/Scripts/Balloon.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Balloon : MonoBehaviour
+{
+
+    public float shakeSpeed;
+    public bool direction = false;
+    private int counter = 0;
+	public int counterMax;
+    // Update is called once per frame
+    void Update()
+    {
+        if (counter < counterMax && direction == false)
+        {
+            transform.position = transform.position + new Vector3(shakeSpeed * Time.deltaTime, 0f);
+            counter++;
+        }
+        else if(direction == false)
+        {
+            direction = true;
+        }
+
+        if (direction == true && counter > 0)
+        {
+            transform.position = transform.position + new Vector3(-shakeSpeed * Time.deltaTime, 0f);
+            counter--;
+        } else if(counter == 0) {
+			direction = false;
+		}
+    }
+}
---
+++ GameJam/Assets/Scripts/Karussell.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Karussell : MonoBehaviour {
+
+	public float rotationSpeed = 15f; 
+
+	void Update () {
+
+		transform.Rotate(Vector3.forward * rotationSpeed * Time.deltaTime);
+
+	}
+}
