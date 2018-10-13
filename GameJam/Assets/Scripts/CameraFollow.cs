7b345c252db315cdfbad6485b1d978b17fe00ba0
Shizobear <K.Tobi@outlook.de>
2018-10-13 10:43:47 +0200

    added riesenrad and physicsmaterial

---
+++ GameJam/Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+
+public class CameraFollow : MonoBehaviour {
+
+	// Use this for initialization
+	public Transform target;
+	public float smoothSpeed = .125f;
+	public Vector3 offset;
+	private void Awake()
+	{
+		target = GameObject.FindGameObjectWithTag("Player").GetComponent<Transform>();
+	}
+
+	private void Start()
+	{
+		offset = new Vector3(0f, 4f, -10f);
+	}
+	private void FixedUpdate()
+	{
+		Vector3 desiredPosition = target.position + offset;
+		Vector3 smoothedPosition = Vector3.Lerp(transform.position, desiredPosition, smoothSpeed);
+		transform.position = smoothedPosition;
+	}
+
+}
