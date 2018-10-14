f6a0fa02a66e77da6f7145643708c5d3eb51d7a1
Shizobear <K.Tobi@outlook.de>
2018-10-14 19:30:19 +0200

    added levelselector

---
+++ GameJam/Assets/Scripts/LevelSelector.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class LevelSelector : MonoBehaviour {
+
+	public Button[] levelButtons;
+	public float levelReached = 0f;
+
+	void Start ()
+	{
+		int levelReached = PlayerPrefs.GetInt("levelReached", 1);
+
+		for (int i = 0; i < levelButtons.Length; i++)
+		{
+			if (i + 1 > levelReached)
+				levelButtons[i].interactable = false;
+		}
+
+	}
+
+	public void Select (string levelName)
+	{
+		SceneManager.LoadScene(levelName);
+	}
+
+
+}
